0f95b1fa5be92fc0c7412256e49203eefd9f1331
winxsst <42728340+winxsst@users.noreply.github.com>
2018-08-31 19:10:39 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -4,6 +4,6 @@ define('APIKEY', 'AIzaSyCNTqQysEuj-7zkzZW5O_2nzAQjizE1lXE');
 define('GJ_CODE', 'US');
 define('SITE_NAME', 'winxss');
 define('TITLENAME', 'DouTube');
-define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'xiaofufua@protonmail.com');
+define('EN2DEKEY', 'ok376485483');
+define('EMAIL', 'winxsst@gmail.com');
 ?>
