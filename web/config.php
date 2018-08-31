fdf3f01297842c38383e8e7aefb0700c5945caf6
winxsst <42728340+winxsst@users.noreply.github.com>
2018-08-31 17:59:11 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
-define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
+define('APIKEY', 'AIzaSyCNTqQysEuj-7zkzZW5O_2nzAQjizE1lXE');
+define('GJ_CODE', 'US');
+define('SITE_NAME', 'winxss');
+define('TITLENAME', 'DouTube');
 define('EN2DEKEY', 'lujianbupingyishenghou');
 define('EMAIL', 'xiaofufua@protonmail.com');
 ?>
